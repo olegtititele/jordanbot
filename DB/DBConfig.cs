9bdb92665597eb00f52b751c56b8fd3cb75689ae
olegtititele <96022759+olegtititele@users.noreply.github.com>
2022-10-17 02:59:48 +0300

    Update DBConfig.cs

--- DB/DBConfig.cs
+++ DB/DBConfig.cs
@@ -2,6 +2,6 @@ namespace PostgreSQL
 {
     public static class DBConfig
     {
-        public static string db = "Host=localhost;Username=parser_user;Password=admin1305.;Database=parser_bot_db";
+        public static string db = "Host=176.124.213.88:5432;Username=parser_user;Password=admin1305.;Database=parser_bot_db";
     }
 }
