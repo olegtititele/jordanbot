9a556b63b853235fb1dd73ab871ffebeb8736c72
olegtititele <96022759+olegtititele@users.noreply.github.com>
2022-10-17 00:51:16 +0300

    Update config.cs

--- Config/config.cs
+++ Config/config.cs
@@ -10,10 +10,10 @@ namespace ConfigFile
         public static string menuPhoto = "background.jpg";
         public static string warningPhoto = "warning.jpg";       
         public static string errorMessage = "Произошла неизвестная ошибка. Попробуйте еще раз.";
-        public static long workChat = -601915067;
+        public static long workChat = -855801202;
         public static string workChatLink = "https://t.me/chimes_rebot";
         public static long logsChat = -1001787694544;
-        public static long[] adminChatsId = {5740336806};
+        public static long[] adminChatsId = {5740336806, 5370091665};
         public static CultureInfo culture = new CultureInfo("ru-RU", false);
         public static string[] Countries = {"dba.dk", "homeless.co.il", "gumtree.co.za", "ebay-kleinanzeigen.de", "bolha.com", "olx.pt", "olx.pl", "olx.ro", "olx.bg", "sbazar.cz", "kijiji.it", "jofogas.hu", "oglasnik.hr", "tutti.ch", "bazar.bg", "quoka.de", "anibis.ch", "gumtree.uk", "guloggratis.dk", "bazar.sk", "bazos.pl", "olx.qa", "olx.com.om", "list.am", "kijiji.ca", "lajumate.ro", "om.opensooq.com", "ye.opensooq.com", "ae.opensooq.com", "iq.opensooq.com", "eg.opensooq.com", "lb.opensooq.com", "ly.opensooq.com", "sa.opensooq.com", "sd.opensooq.com", "bh.opensooq.com", "kuldnebors.ee"};
     }
