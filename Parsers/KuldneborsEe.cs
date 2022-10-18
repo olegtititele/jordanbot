fa9c930b9bb406eceb459bce7e96ae8328a5a1af
olegtititele <96022759+olegtititele@users.noreply.github.com>
2022-10-18 22:13:32 +0300

    Update KuldneborsEe.cs

--- Parsers/KuldneborsEe.cs
+++ Parsers/KuldneborsEe.cs
@@ -25,11 +25,11 @@ namespace Parser
             options.AddArgument("--disable-dev-shm-usage");
             options.AddArgument($"--user-agent={userAgent}");
             options.AddArguments("--headless");
-            options.AddArgument("--window-size=1920,1080")
-            options.AddArgument("--disable-extensions")
-            options.AddArgument("--start-maximized")
-            options.AddArgument('--disable-gpu')
-            options.AddArgument('--ignore-certificate-errors')
+            options.AddArgument("--window-size=1920,1080");
+            options.AddArgument("--disable-extensions");
+            options.AddArgument("--start-maximized");
+            options.AddArgument("--disable-gpu");
+            options.AddArgument("--ignore-certificate-errors");
             IWebDriver driver = new FirefoxDriver(options);
             driver.Manage().Window.FullScreen();
 
