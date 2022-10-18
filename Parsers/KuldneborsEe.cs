b2ee0234e1d478ae5e8e9ae5f904041e398e71f2
olegtititele <96022759+olegtititele@users.noreply.github.com>
2022-10-19 01:43:46 +0300

    Update KuldneborsEe.cs

--- Parsers/KuldneborsEe.cs
+++ Parsers/KuldneborsEe.cs
@@ -166,6 +166,10 @@ namespace Parser
                 
                 IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
                 js.ExecuteScript(getPhoneScript);
+                js.ExecuteScript(getPhoneScript);
+                js.ExecuteScript(getPhoneScript);
+                js.ExecuteScript(getPhoneScript);
+                js.ExecuteScript(getPhoneScript);
 //                 // js.ExecuteScript("arguments[0].scrollIntoView()", ifPhoneContainsNumber); 
 //                 // js.ExecuteScript("arguments[0].click();", ifPhoneContainsNumber);
 //                 // js.ExecuteScript("arguments[0].click();", ifPhoneContainsNumber);
@@ -174,7 +178,7 @@ namespace Parser
 //                 // ifPhoneContainsNumber.Click();
 //                 // ifPhoneContainsNumber.Click();
 //                     driver.FindElement(By.XPath("//span[@id=\"contact-phones\"]")).Click();
-                    System.Threading.Thread.Sleep(3000);
+                    System.Threading.Thread.Sleep(60000);
                 Console.WriteLine(driver.FindElement(By.XPath("//span[@id=\"contact-phones\"]")).GetAttribute("innerHTML"));
 
 //                 sellerPhoneNumber = driver.FindElement(By.XPath("//span[@id=\"contact-phones\"]")).Text.Trim().Replace(" ", "");
