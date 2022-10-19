6531d2c9a30120e2e6827d4a98273968a71895c9
olegtititele <96022759+olegtititele@users.noreply.github.com>
2022-10-19 03:31:02 +0300

    Update KuldneborsEe.cs

--- Parsers/KuldneborsEe.cs
+++ Parsers/KuldneborsEe.cs
@@ -25,9 +25,9 @@ namespace Parser
             var options = new FirefoxOptions();
             options.AddArgument("--no-sandbox");
             options.AddArgument("--disable-dev-shm-usage");
-//             options.AddArgument($"--user-agent={userAgent}");
+            options.AddArgument($"--user-agent={userAgent}");
 //             options.AddArgument("--disable-gpu");
-//             options.AddArguments("--headless");
+            options.AddArguments("--headless");
 //             options.AddArguments("--start-maximized");
             // options.SetPreference("permissions.default.image", 2);
             IWebDriver driver = new FirefoxDriver(options);
