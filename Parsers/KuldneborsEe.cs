74f73d50256f5447cfaf3d0d5b11affdcd989390
olegtititele <96022759+olegtititele@users.noreply.github.com>
2022-10-20 18:16:25 +0300

    Update KuldneborsEe.cs

--- Parsers/KuldneborsEe.cs
+++ Parsers/KuldneborsEe.cs
@@ -10,7 +10,7 @@ namespace Parser
         private static int annoounCount = 0;
         private static string userAgent = "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1";
         private static string errorImageUri = "https://upload.wikimedia.org/wikipedia/commons/9/9a/%D0%9D%D0%B5%D1%82_%D1%84%D0%BE%D1%82%D0%BE.png";
-        private static string goodLink = "https://www.kuldnebors.ee/search/kodu/mooblidetailid/mooblipadjad/search.mec?pob_post_id=85609045&pob_action=show_post&pob_cat_id=11636&pob_browser_offset=30&pob_view_language_id=et&search_evt=onsearch&search_O_string=ipad&search_O_user_types=-R&pob_page_index=1";
+        private static string goodLink = "https://www.kuldnebors.ee/search/telefonid/mobiiltelefonide-lisaseadmed/iphone-13-pro-back-case/search.mec?pob_post_id=86525661&pob_action=show_post&pob_cat_id=10946&pob_browser_offset=30&pob_view_language_id=et&search_evt=onsearch&search_O_string=Apple&search_O_user_types=-R&pob_page_index=1";
         private static int pagesPassed = 1;
         private static int adsPassed = 0;
         
@@ -222,13 +222,16 @@ namespace Parser
 //                     }
                     
 //                     if(!result){ return; }
+                    js.ExecuteScript($"arguments[0].setAttribute(\"onclick\", \"{script}\");", driver.FindElement(By.XPath("//h1[@class=\"kb-object-view__heading\"]")));
                     js.ExecuteScript($"arguments[0].setAttribute(\"onclick\", \"{script}\");", driver.FindElement(By.XPath("//*[@id=\"contact-phones\"]/a")));
                     System.Threading.Thread.Sleep(1000);
                     Console.WriteLine(script);
                     Console.WriteLine(driver.FindElement(By.XPath("//*[@id=\"contact-phones\"]")).GetAttribute("innerHTML"));
-                    js.ExecuteScript(script.Split(';')[0]);
-                    js.ExecuteScript(script.Split(';')[0]);
-                    js.ExecuteScript(script.Split(';')[0]);
+                    driver.FindElement(By.XPath("//h1[@class=\"kb-object-view__heading\"]")).Click();
+                    driver.FindElement(By.XPath("//h1[@class=\"kb-object-view__heading\"]")).Click();
+//                     js.ExecuteScript(script.Split(';')[0]);
+//                     js.ExecuteScript(script.Split(';')[0]);
+//                     js.ExecuteScript(script.Split(';')[0]);
 //                     js.ExecuteScript(script);
 //                     js.ExecuteScript(script);
                     System.Threading.Thread.Sleep(1000);
